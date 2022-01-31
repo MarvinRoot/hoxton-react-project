a477518704f043e28706e01a520448ce2b839c95
Marvin Aliaj <74553145+MarvinRoot@users.noreply.github.com>
2022-01-31 14:59:17 +0100

    Got some of the routes ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Main } from './components/pages/Main'
+import { SignIn } from './components/pages/SignIn'
+import { SignUp } from './components/pages/SignUp'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route index element={<Navigate replace to="/sign-in" />} />
+        <Route path='/sign-in' element={< SignIn />} />
+        <Route path='/sign-up' element={< SignUp />} />
+        <Route path='/main' element={< Main />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
     </div>
   )
 }
