bf1c313a1d1809d4769e9c858746e89d0a7d1251
Marvin Aliaj <74553145+MarvinRoot@users.noreply.github.com>
2022-01-31 19:10:25 +0100

    Finished styling the sign-in page

--- src/components/pages/SignIn.jsx
+++ src/components/pages/SignIn.jsx
@@ -1,5 +1,32 @@
+import { useNavigate } from "react-router-dom"
+
 export function SignIn() {
+    const navigate = useNavigate()
     return (
-        <h1>Sign In Page</h1>
+        <div className="sign-in">
+            <h1 className="logo">Hoxtify</h1>
+            <h2>Your daily music dose</h2>
+            <form>
+                <div className="container">
+                    <h1>Hey there!</h1>
+
+                    <label>
+                        <span>Enter your email adress</span>
+                        <input required type="email" placeholder="Enter your email adress" />
+                    </label>
+
+                    <label>
+                        <span>Enter your password</span>
+                        <input required type="password" placeholder="Enter your password" />
+                    </label>
+
+                    <button type="submit" value="Submit">
+                        Sign In
+                    </button>
+
+                    <p onClick={() => navigate('/sign-up')}>Don't have an account? Create one</p>
+                </div>
+            </form>
+        </div>
     )
 }
