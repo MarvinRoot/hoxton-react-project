294e6e753e0362bccfc80a9f08938b44cbf5c33a
Marvin Aliaj <74553145+MarvinRoot@users.noreply.github.com>
2022-02-03 13:14:28 +0100

    Finished updating the database with new song data

--- src/pages/components/MainPage.jsx
+++ src/pages/components/MainPage.jsx
@@ -1,8 +1,15 @@
+import React from "react"
+import ReactPlayer from "react-player"
+
 export function MainPage() {
     return (
         <section className="main-page-content">
             <div className="music-tracks">
                 <h1>Music For You</h1>
+                {/* <ReactPlayer
+        url="https://soundcloud.com/glennmorrison/beethoven-moonlight-sonata"
+      /> */}
+          <iframe width="400px" height="250" scrolling="no" frameborder="no" allow="autoplay" src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1150594213&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'></iframe>
             </div>
             <div className="podcasts">
                 <h1>Popular Podcasts</h1>
